68a886ccfdd7fdce16e7963cf56050bfd10f0d77
ironpack <rcamacho0198@gmail.com>
2023-03-04 13:43:22 -0700

    Added ticking

--- gears.js
+++ gears.js
@@ -1,42 +1,54 @@
 const numBoxes = 6;
-var boxes = [];
-var coordinates = [];
-const speed = 0.5;
-var radius = Math.min(window.innerWidth, window.innerHeight) * 0.3; // calculate the radius based on the screen size
-function wait(time)
+const boxes = [];
+const coordinates = [];
+const speed = 0.1;
+const DEG_TO_RAD = Math.PI / 180;
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+let radius = Math.min(window.innerWidth, window.innerHeight) * 0.3;
+var myInterval = setInterval(tick, 0);
+var lastUpdate = Date.now();
+var deltaTime =  Date.now() - lastUpdate;
+
+function tick()
 {
-    return new Promise(resolve => setTimeout(resolve, time));
+    var now = Date.now();
+    this.deltaTime = now - lastUpdate;
+    lastUpdate = now;
 }
 
-function speen(box, offset)
+function speen(box, offset) 
 {
-    var angle = coordinates[offset];
-    function updateBox()
+    let angle = coordinates[offset];
+
+    function updateBox() 
     {
-        angle += (Math.PI / 180) * speed;
-        var x = window.innerWidth / 2 + radius * Math.cos(angle);
-        var y = window.innerHeight / 2 + radius * Math.sin(angle);
+        angle += DEG_TO_RAD * speed * deltaTime;
+        let x = windowWidth / 2 + radius * Math.cos(angle);
+        let y = windowHeight / 2 + radius * Math.sin(angle);
         box.style.left = x + 'px';
         box.style.top = y + 'px';
         coordinates[offset] = angle;
         requestAnimationFrame(updateBox);
     }
+
     updateBox();
 }
 
-async function main()
+function main() 
 {
-    for(i = 0; i < numBoxes; i++)
+    for(let i = 0; i < numBoxes; i++) 
     {
         boxes.push(document.getElementById("box" + (i+1)));
-        var offsetAngle = i * (2 * Math.PI / numBoxes);
-        coordinates.push(offsetAngle);
+        coordinates.push(i * (2 * Math.PI / numBoxes));
         speen(boxes[i],i);
     }
 }
 
 main();
-window.onresize = function(){ 
-  radius = Math.min(window.innerWidth, window.innerHeight) * 0.4; // update the radius based on the new screen size
-  location.reload(); 
-}
+
+window.onresize = function()
+{ 
+    radius = Math.min(window.innerWidth, window.innerHeight) * 0.4; // update the radius based on the new screen size
+    location.reload(); 
+  } 
